034c6faafe3f6f5df943a561d525eb268b5eb4f2
jimurphy@adobe.com <jimurphy@adobe.com@a9308255-753e-0410-a2e9-80b3fbc4fff6>
2009-02-05 18:51:23 +0000

    Merge i10 revisions 4827,4834,4837,4845,4859-4860 -> trunk
    git-svn-id: http://opensource.adobe.com/svn/opensource/flex/sdk/trunk/modules/asc@4862 a9308255-753e-0410-a2e9-80b3fbc4fff6

--- src/java/macromedia/asc/semantics/FlowAnalyzer.java
+++ src/java/macromedia/asc/semantics/FlowAnalyzer.java
@@ -333,7 +333,7 @@ public final class FlowAnalyzer extends Emitter implements Evaluator, ErrorConst
                         ns = ((value instanceof ObjectValue) ? (ObjectValue) value : null);
                         if( ns == null )
                         {
-                            ns = cx.getUnresolvedNamespace(cx, node, (ReferenceValue) ref);
+                            return null;
                             // cx.error(node.qualifier.pos(), kError_UndefinedNamespace );
                         }
                     }
