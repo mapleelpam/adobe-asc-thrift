b43e8eaee1cacf3e1988af11f06c2be704a3e7d5
Kai-Feng <maple@maple-asus.(none)>
2011-04-04 23:26:18 +0800

    update build method and integrate with cmake (outside cmake)

--- thrift_test/cpp-test/SyntaxTree.thrift
+++ thrift_test/cpp-test/SyntaxTree.thrift
@@ -1,1 +1,37 @@
-../../SyntaxTree.thrift
+
+namespace java tw.maple.generated
+namespace cpp ast.dumper
+
+typedef list<string> StringList
+
+enum ExpressionType
+{
+    IDENTIFIER,
+    CALL
+}
+
+enum IdentifierType
+{
+    TYPE_IDENTIFIER,
+    ATTR_IDENTIFIER,
+    IDENTIFIER, 
+}
+
+struct Identifier
+{
+    1: IdentifierType type,
+    2: string name,
+}
+
+struct CallExpression
+{
+    1: bool is_new = 0,
+}
+
+service AstDumper
+{
+
+    oneway void ping(),
+    oneway void ping2( 1: i32 echo ),
+}
+
